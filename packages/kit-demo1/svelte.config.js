02c7b77e0b59815a5fa0e10b94b9054b86634115
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2023-12-04 16:11:19 +0900

    configure to disable cross origin check

--- packages/kit-demo1/svelte.config.js
+++ packages/kit-demo1/svelte.config.js
@@ -1,17 +1,20 @@
-import adapter from "@sveltejs/adapter-node";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+	preprocess: vitePreprocess(),
 
-  kit: {
-    paths: {
-      relative: false,
-      base: "/kit-demo1",
-    },
-    adapter: adapter(),
-  },
+	kit: {
+		csrf: {
+			checkOrigin: false
+		},
+		paths: {
+			relative: false,
+			base: '/kit-demo1'
+		},
+		adapter: adapter()
+	}
 };
 
 export default config;
